9e825ca738bb0e70ae385bf05af39df344b2d994
jbrownssf <jpbrown@softstackfactory.org>
2017-05-10 19:26:45 +0000

    Create Access Token

--- common/models/app-users.js
+++ common/models/app-users.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Appusers) {
-
+module.exports = function(AppUsers) {
+    
+    AppUsers.observe('after save', function(ctx, next) {
+        if(ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) { 
+                if(err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }else {
+             next();
+        }
+    });
+    
 };
